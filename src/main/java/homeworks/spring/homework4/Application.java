31d29da8c0caa2e5f4f80633227d1862aca02fbb
Dmitri <datitov2009@yandex.ru>
2024-01-28 11:19:45 +0300

    add reader properties class

--- src/main/java/homeworks/spring/homework4/Application.java
+++ src/main/java/homeworks/spring/homework4/Application.java
@@ -1,10 +1,14 @@
 package homeworks.spring.homework4;
 
+import homeworks.spring.homework4.service.ReaderProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.domain.PageRequest;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ReaderProperties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
